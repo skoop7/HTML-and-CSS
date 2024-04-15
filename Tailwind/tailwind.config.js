cc5d96e81197ab2ea95b32db54307cd46a75560e
skoop7 <skobarne7@gmail.com>
2024-04-16 01:24:06 +0530

    This is my portfolio using HTML and Tailwind CSS

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        customGreen: "#ebf5ee",
+        customBrown: '#bfa89e',
+        customGray: '#8b786d',
+      },
+    },
+  },
+  plugins: [],
+};
